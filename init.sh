0c83c9945aaaa8afff710d12241dc06952e287e5
Pavel Varfalameev <webreeo@gmail.com>
2020-05-04 14:33:13 +0500

    upd

--- init.sh
+++ init.sh
@@ -17,14 +17,15 @@ fi
 
 sudo openssl req -nodes -days 3650 -x509 -newkey rsa:2048 -keyout ./private.key -out ./cert.crt -subj '/CN=www.test.com/O=My Company Name LTD./C=US'
 
+sudo docker volume create --name=nexus-data
 sudo docker-compose up -d
 
 until curl --fail --insecure https://localhost; do
-  sleep 5
   echo "Wait..."
+  sleep 5
 done
 
-curl -X POST -u "admin:admin123" --insecure "https://127.0.0.1/service/rest/beta/repositories/docker/hosted" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"name\": \"docker_repo\", \"online\": true, \"storage\": { \"blobStoreName\": \"default\", \"strictContentTypeValidation\": true, \"writePolicy\": \"ALLOW_ONCE\" }, \"docker\": { \"v1Enabled\": false, \"forceBasicAuth\": true, \"httpPort\": 5000 }}"
 password=$(sudo docker-compose exec nexus cat /nexus-data/admin.password)
+curl -X POST -u "admin:$password" --insecure "https://127.0.0.1/service/rest/beta/repositories/docker/hosted" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"name\": \"docker_repo\", \"online\": true, \"storage\": { \"blobStoreName\": \"default\", \"strictContentTypeValidation\": true, \"writePolicy\": \"ALLOW_ONCE\" }, \"docker\": { \"v1Enabled\": false, \"forceBasicAuth\": true, \"httpPort\": 5000 }}"
 sleep 5
 echo "Admin password is: $password"
